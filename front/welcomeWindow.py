263b2f57430e6a4049e48a463d905f29485d773f
hardKokos <102967897+hardKokos@users.noreply.github.com>
2023-06-06 13:46:06 +0200

    Added all windows and also functioning buttons. Need to repair alignment of widgets.

--- front/welcomeWindow.py
+++ front/welcomeWindow.py
@@ -1,16 +1,38 @@
 from kivy.app import App
-from kivy.uix.widget import Widget
+from kivy.uix.screenmanager import ScreenManager, Screen
 
 
-class Welcome(Widget):
+class Welcome(Screen):
+    pass
 
-    def button_pressed(self):
-        pass
+
+class ChoosePerson(Screen):
+    pass
+
+
+class ChooseRecommendationMethod(Screen):
+    pass
+
+
+class LoadingScreen(Screen):
+
+    def start_loading(self):
+        app = App.get_running_app()
+        screen_manager = app.root.ids.screen_manager
+        screen_manager.current = 'recommended'
+
+
+class RecommendedProducts(Screen):
+    pass
+
+
+class WindowManager(ScreenManager):
+    pass
 
 
 class RecommendApp(App):
     def build(self):
-        return Welcome()
+        return WindowManager()
 
 
 if __name__ == '__main__':
