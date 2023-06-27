01096ba66d3f0e5e5a26c1b4a4092d78cca6f08e
Jakub Mijas <45014798+DevMias@users.noreply.github.com>
2023-06-28 01:03:26 +0200

    Update similarity_recomendation.py
    Updated file to work as a function and retrurn list of tuples as a result

--- backend/similarity_recomendation.py
+++ backend/similarity_recomendation.py
@@ -4,27 +4,35 @@ import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-with open('products.json') as f:
-    data = json.load(f)
+def recommend_products(product_name, cosine):
+    with open('products.json') as f:
+        data = json.load(f)
         
-data = pd.DataFrame(data)
+    data = pd.DataFrame(data)
 
+    tfidf = TfidfVectorizer(stop_words='english')
+    tfidf_matrix = tfidf.fit_transform(data['tags'])
+    cosine_sim = cosine_similarity(tfidf_matrix)
 
-tfidf = TfidfVectorizer(stop_words='english')
-tfidf_matrix = tfidf.fit_transform(data['tags'])
-cosine_sim = cosine_similarity(tfidf_matrix)
-
-def recommend_products(title,cosine):
-    index = data[data['title'] == title].index[0]
+    index = data[data['title'] == product_name].index[0]
     dist = list(enumerate(cosine[index]))
     dist.sort(reverse=True, key=lambda x: x[1])
+    recommendations = []
     for i in dist[1:5]:
+        similar_product = data.iloc[i[0]]['title']
         similarity_value = i[1]
-        print(data.iloc[i[0]]['title'], similarity_value)
+        recommendations.append((similar_product, similarity_value))
+    return recommendations
 
-def get(cosine):
-    product = input("Enter product name: ")
-    print(f"\nRecommended products after buying {product}: ")
-    recommend_products(product,cosine)
+def get_recommendations(product_name):
+    with open('products.json') as f:
+        data = json.load(f)
+        
+    data = pd.DataFrame(data)
+
+    tfidf = TfidfVectorizer(stop_words='english')
+    tfidf_matrix = tfidf.fit_transform(data['tags'])
+    cosine_sim = cosine_similarity(tfidf_matrix)
     
-get(cosine_sim)
+    recommendations = recommend_products(product_name, cosine_sim)
+    return recommendations
