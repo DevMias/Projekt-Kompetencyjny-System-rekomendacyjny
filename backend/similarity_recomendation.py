1eaa5cba6a281e5638ab26d14707c1e9fae2362b
Jakub Mijas <45014798+DevMias@users.noreply.github.com>
2023-06-28 02:46:47 +0200

    Update similarity_recomendation.py
    Changed function to work on reviewers id

--- backend/similarity_recomendation.py
+++ backend/similarity_recomendation.py
@@ -4,60 +4,43 @@ import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-def get_last_asin_by_reviewer_id(reviewer_id):
-    with open('../database/reviews.json') as f:
-        data = json.load(f)
-        
-    asin_list = []
-    for record in data:
-        if record['reviewerID'] == reviewer_id:
-            asin_list.append(record['asin'])
-    
-    if asin_list:
-        return asin_list[-1]
-    else:
+def get_similarity_recommendations_by_reviewer_id(reviewer_id):
+    with open('reviews.json') as f:
+        reviews_data = json.load(f)
+    with open('products.json') as f:
+        products_data = json.load(f)
+
+    reviews = pd.DataFrame(reviews_data)
+    products = pd.DataFrame(products_data)
+
+    user_reviews = reviews[reviews['reviewerID'] == reviewer_id]
+    if user_reviews.empty:
         return None
 
-def get_product_name(asin):
-    with open('../database/products.json') as f:
-        products = json.load(f)
-    for product in products:
-        if product['asin'] == asin:
-            return product['title']
-    
-    return None
-
-
-
-def recommend_products(product_name, cosine):
-    with open('../database/products.json') as f:
-        data = json.load(f)
-        
-    data = pd.DataFrame(data)
+    user_last_asin = user_reviews['asin'].iloc[-1]
+    product_name = products[products['asin'] == user_last_asin]['title'].iloc[0]
 
     tfidf = TfidfVectorizer(stop_words='english')
-    tfidf_matrix = tfidf.fit_transform(data['tags'])
+    tfidf_matrix = tfidf.fit_transform(products['tags'])
     cosine_sim = cosine_similarity(tfidf_matrix)
 
-    index = data[data['title'] == product_name].index[0]
-    dist = list(enumerate(cosine[index]))
+    product_index = products[products['title'] == product_name].index[0]
+    dist = list(enumerate(cosine_sim[product_index]))
     dist.sort(reverse=True, key=lambda x: x[1])
     recommendations = []
     for i in dist[1:5]:
-        similar_product = data.iloc[i[0]]['title']
+        similar_product = products.iloc[i[0]]['title']
         similarity_value = i[1]
         recommendations.append((similar_product, similarity_value))
     return recommendations
 
-def get_recommendations(product_name):
-    with open('../database/products.json') as f:
-        data = json.load(f)
-        
-    data = pd.DataFrame(data)
+# Example usage
+#reviewer_id = 'A1727O77XSE5QQ'
+#recommendations = get_recommendations_by_reviewer_id(reviewer_id)
 
-    tfidf = TfidfVectorizer(stop_words='english')
-    tfidf_matrix = tfidf.fit_transform(data['tags'])
-    cosine_sim = cosine_similarity(tfidf_matrix)
-    
-    recommendations = recommend_products(product_name, cosine_sim)
-    return recommendations
+#if recommendations:
+#    print(f"Recommended products for reviewer ID '{reviewer_id}':")
+#    for product, score in recommendations:
+#        print(f"Product: {product}, Score: {score}")
+#else:
+#    print(f"No recommendations found for reviewer ID '{reviewer_id}'.")
